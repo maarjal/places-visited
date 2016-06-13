cc844cfa633938a3001e1128154ebf808db53987
Joel Delight and Maarja Laineste <maarja@>
2016-06-13 08:41:43 -0700

    complete the third BDD goal- add season of visit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
-function Vacation(loc, landmark) {
+function Vacation(loc, landmark, season) {
   this.loc = loc;
   this.landmark = [];
   this.landmark.push(landmark);
-  alert(this.loc)
+  this.season = season;
+  alert(this.loc);
 };
 
 Vacation.prototype.addLandmark = function(landmark) {
