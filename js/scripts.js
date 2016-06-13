66a8f5b35f29c439f33e60e5b99852786cc74c95
Joel Delight and Maarja Laineste <maarja@>
2016-06-13 14:28:39 -0700

    fix the ability to add a second landmark

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-var currentVacation = "";
-
+var vacationList = [];
+var numberOfVacations = 0;
+var currentSelection = 0;
 function Vacation(loc, landmark, season, note) {
   this.loc = loc;
   this.landmark = [];
   this.landmark.push(landmark);
   this.season = season;
   this.note = note;
-  // debugger;
 };
 
 Vacation.prototype.addLandmark = function(landmark) {
@@ -16,29 +16,29 @@ Vacation.prototype.addLandmark = function(landmark) {
 $(document).ready(function() {
   $("form#new-place").submit(function(event) {
     event.preventDefault();
-
     var loc = $("input#new-location").val();
     var landmark = $("input#new-landmark").val();
     var season = $("select#new-season").val();
     var note = $("input#new-note").val();
-
     var newVacation = new Vacation(loc, landmark, season, note);
-
-    $("#places").append("<li> <span class='vacation'>" + newVacation.loc + "</span></li>");
-    // debugger;
+    vacationList.push(newVacation);
+    var currentVacation = "vacation" + numberOfVacations
+    console.log(currentVacation);
+    $("#places").append("<li> <span id=" + currentVacation + " class='vacation'>" + newVacation.loc + "</span></li>");
+    numberOfVacations ++;
     $(".vacation").last().click(function() {
-      currentVacation =
+      currentSelection = this.id.slice(-1);
        $("#show-places").show();
        $("#show-places h2").text(newVacation.loc);
        $("#show-landmark").text(newVacation.landmark);
        $("#show-season").text(newVacation.season);
        $("#show-note").text(newVacation.note);
     });
-
   });
-  $("form#landmark").submit(function(event) {
+  $("#landmark").submit(function(event) {
     event.preventDefault();
+    console.log("made it here");
     var addedLandmark = $("input#add-landmark").val();
-    addLandmark(addedLandmark);
+    vacationList[currentSelection].addLandmark(addedLandmark);
   });
 });
